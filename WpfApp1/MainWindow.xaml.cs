d109cc3bbca541720dcfb2e13c7f1c50f701da78
Сергей Даценко <dasergiy@gmail.com>
2022-03-09 16:41:12 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>Interaction logic for MainWindow.xaml</summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.AppendText("Hello world! " + DateTime.UtcNow + Environment.NewLine);
+        }
+    }
+}
